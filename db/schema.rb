0c67f69f7dbb4b04728dc223906ab7ba8664b63a
Jeffery <522023893@qq.com>
2021-04-15 08:55:14 +0800

    实现消息存储提取功能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_060817) do
+ActiveRecord::Schema.define(version: 2021_04_14_095332) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "published_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "texts", force: :cascade do |t|
     t.string "uname"
