6b9445ffd9740714a96a6fe391a8fa26aeb2504b
Jeffery <522023893@qq.com>
2021-04-20 11:09:32 +0800

    before study

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -2,43 +2,7 @@ class ReceiveController < ApplicationController
 before_action :set_name, only: %i[show]
 before_action :get_num, only: %i[index]
 def new
-  @pieSize = {
-    :height => 500,
-    :width => 500
-  }
-  @pieData = [
-    {
-      value: 300,
-      color:"#F7464A",
-      highlight: "#FF5A5E",
-      label: "Red"
-    },
-    {
-      value: 50,
-      color: "#46BFBD",
-      highlight: "#5AD3D1",
-      label: "Green"
-    },
-    {
-      value: 100,
-      color: "#FDB45C",
-      highlight: "#FFC870",
-      label: "Yellow"
-    },
-    {
-      value: 40,
-      color: "#949FB1",
-      highlight: "#A8B3C5",
-      label: "Grey"
-    },
-    {
-      value: 120,
-      color: "#4D5360",
-      highlight: "#616774",
-      label: "Dark Grey"
-    }
-
-  ].to_json
+ 
 end
 
 def show
